3dd12d4967a189bf6bff96a0ee7da97e1e3fe2aa
Leigh Van Belling <leighannvanbelling@leighs-macbook-pro.local>
2018-06-10 15:20:43 +0200

    Convert to MVP Design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Leigh Ann Van Belling on 2018/06/10.
+//  Copyright © 2018 Leigh Ann Van Belling. All rights reserved.
+//
+
+import Foundation
+class Item{
+
+        var title : String = ""
+        var done : Bool = false
+    
+}
